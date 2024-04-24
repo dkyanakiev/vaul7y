4bf2c8911bda171fd0020560b84cd4057f2b37e8
Dimitar Yanakiev <31411471+dkyanakiev@users.noreply.github.com>
2024-04-24 22:36:02 +0300

    Fallback method for mount listing (#16)
    * Fix for mounts lookup
    
    * Changing function to ReadRaw
    
    * cleanup
    
    * Changelog update

--- internal/vault/mounts.go
+++ internal/vault/mounts.go
@@ -1,20 +1,20 @@
 package vault
 
 import (
+	"encoding/json"
 	"fmt"
-	"strings"
+	"io"
 
 	"github.com/dkyanakiev/vaulty/internal/models"
 	"github.com/hashicorp/vault/api"
 )
 
 func (v *Vault) ListMounts() (map[string]*models.MountOutput, error) {
+
 	apiMountList, err := v.vault.Sys().ListMounts()
 	if err != nil {
-		if strings.Contains(err.Error(), "route entry not found") {
-			return make(map[string]*models.MountOutput), nil
-		}
-		return nil, fmt.Errorf("failed to retrieve secret mounts: %w", err)
+		v.Logger.Warn().Err(err).Msg("Unable to access sys/mounts, attempting to use fallback method.\n")
+		return v.listMountsFallback()
 	}
 
 	// Convert api.MountOutput to MountOutput
@@ -22,9 +22,35 @@ func (v *Vault) ListMounts() (map[string]*models.MountOutput, error) {
 	for k, v := range apiMountList {
 		mountList[k] = toMount(v)
 	}
-
 	return mountList, nil
 
+}
+
+func (v *Vault) listMountsFallback() (map[string]*models.MountOutput, error) {
+
+	resp, err := v.vault.Logical().ReadRaw("/sys/internal/ui/mounts")
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve secret mounts: %w", err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var response models.UiMountsResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+
+	// Convert models.MountOutput to api.MountOutput
+	mountList := make(map[string]*models.MountOutput)
+	for k, v := range response.Data.Secret {
+		mountList[k] = v
+	}
+
+	return mountList, nil
 }
 
 func (v *Vault) AllMounts() (map[string]*models.MountOutput, error) {
